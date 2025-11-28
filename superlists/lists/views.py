8a1f016406fb716eb1812770ad5e15836789d037
михаил <maksutenkomihail@gmail.com>
2025-11-28 21:14:22 +0300

    Модель для элементов списка и связанная с ней миграция

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,5 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
     """Домашняя страница"""
-    return render(request, "home.html")
+    # if request.method == "POST":
+    #     return HttpResponse()
+    return render(
+        request,
+        "home.html",
+        {"new_item_text": request.POST.get("item_text")},
+    )
