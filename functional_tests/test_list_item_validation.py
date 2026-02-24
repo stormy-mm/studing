cf8da42b30cc7a7ed2c91709a386bb2fcbfbc8f0
stormy_mm <maksutenkomihail@gmail.com>
2026-02-24 18:48:12 +0300

    добавлен ФТ на валидацию элементов списка

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,3 +1,6 @@
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+
 from .base import FunctionalTest
 
 
@@ -8,15 +11,32 @@ class ItemValidationTest(FunctionalTest):
         """Тест: нельзя добавить пустой список"""
         # Эдит открывает домашнюю страницу и случайно пытается отправить
         # пустой элемент. Она нажимает энтер на пустом поле ввода
+        self.browser.get(self.live_server_url)
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
 
         # Домашняя страница обновляется, и появляется сообщение об ошибке,
         # которое говорит, что элементы списка не должны быть пустыми
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element(By.CSS_SELECTOR, ".has-error").text,
+            "You can't have an empty list item"
+        ))
 
         # Она пробует снова, теперь с неким текстом для элемента, и теперь это срабатывает
+        self.browser.find_element(By.ID, "id_new_item").send_keys("Buy milk")
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy milk")
 
         # Как ни странно, Эдит решает отправить пустой элемент списка
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
 
         # Она получает аналогичное предупреждение на странице списка
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element(By.CSS_SELECTOR, ".has-error").text,
+            "You can't have an empty list item"
+        ))
 
         # И она может меня исправить, заполнив поле неким тестом
-        self.fail("напиши меня!")
+        self.browser.find_element(By.ID, "id_new_item").send_keys("Make tea")
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy milk")
+        self.wait_for_row_in_list_table("2: Make tea")
