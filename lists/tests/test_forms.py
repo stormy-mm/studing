87d61b3a4b6b749826651b83288f80164a5a3536
stormy_mm <maksutenkomihail@gmail.com>
2026-03-03 01:28:06 +0300

    Переопределён метод проверки уникальности

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR, ExistingListItemForm
 from lists.models import Item, List
 
 
@@ -30,3 +30,28 @@ class ItemFormTest(TestCase):
         self.assertEqual(new_item, Item.objects.first())
         self.assertEqual(new_item.text, "do me")
         self.assertEqual(new_item.list, list_)
+
+
+class ExistingListItemFormTest(TestCase):
+    """Тест формы для существующего списка"""
+
+    def test_form_renders_item_text_input(self):
+        """Тест: форма отображает текстовый ввод элемента"""
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_)
+        self.assertIn('placeholder="Enter a to-do item"', form.as_p())
+
+    def test_form_validation_for_blank_items(self):
+        """Тест: валидация формы для пустых элементов"""
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={"text": ""})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors["text"], [EMPTY_ITEM_ERROR])
+
+    def test_form_validation_for_duplicate_items(self):
+        """Тест: валидация формы для повторных элементов"""
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text="to do")
+        form = ExistingListItemForm(for_list=list_, data={"text": "to do"})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors["text"], [DUPLICATE_ITEM_ERROR])
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,7 +1,9 @@
+from unittest import skip
+
 from django.utils.html import escape
 from django.test import TestCase
 
-from ..forms import ItemForm, EMPTY_ITEM_ERROR
+from ..forms import ItemForm, EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR
 from ..models import Item, List
 
 
@@ -136,6 +138,21 @@ class ListViewTest(TestCase):
         """Тест на недопустимый ввод: на странице показывается ошибка"""
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        """Тест: ошибки валидации повторяющегося элемента оканчиваются на странице списка"""
+        list1 = List.objects.create()
+        Item.objects.create(list=list1, text="textey")
+        response = self.client.post(
+            f"/lists/{list1.id}/",
+            data={"text": "textey"}
+        )
+
+        self.assertContains(response, escape(DUPLICATE_ITEM_ERROR))
+        self.assertTemplateUsed(response, "list.html")
+        self.assertEqual(Item.objects.all(), 1)
+
 
 class HomePageTest(TestCase):
     """Тест домашней страницы"""
