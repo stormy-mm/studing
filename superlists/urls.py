a1dabae52094707d0323017ca33d3ad9bcda5215
stormy_mm <maksutenkomihail@gmail.com>
2026-04-27 16:50:00 +0300

    зарегистрирован accounts

--- superlists/urls.py
+++ superlists/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.urls import path, include
 from lists import views as list_views, urls as list_urls
 
+from accounts import urls as account_urls
+
 urlpatterns = [
     path('', list_views.HomePageView.as_view(), name="home"),
     path("lists/", include(list_urls)),
+    path("accounts/", include(account_urls)),
 ]
-
