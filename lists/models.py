dff5128421bcfedb170d4393193ea9634bdabb96
stormy_mm <maksutenkomihail@gmail.com>
2026-04-27 16:53:07 +0300

    отступы

--- lists/models.py
+++ lists/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+
 class List(models.Model):
     """Список"""
 
     def get_absolute_url(self):
         return reverse("view_list", args=[self.id])
+
 
 class Item(models.Model):
     """Элемент списка"""
